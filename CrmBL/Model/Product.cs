8005db125cfadd895cfebd6a3db093c569c7ac48
Denys Kachalenko <kachalenko.denis@gmail.com>
2022-10-18 15:21:25 +0300

    Add a cart model and unit tests.

--- CrmBL/Model/Product.cs
+++ CrmBL/Model/Product.cs
@@ -14,5 +14,19 @@ namespace CrmBL.Model
         {
             return Name;
         }
+
+        public override int GetHashCode()
+        {
+            return ProductId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product product)
+            {
+                return ProductId.Equals(product.ProductId);
+            }
+            return false;
+        }
     }
 }
---
+++ CrmBLTests/Model/CartTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CartTest()
+        {
+            Customer customer = new Customer()
+            {
+                CustomerId = 1,
+                Name = "Test User"
+            };
+            Product product1 = new Product()
+            {
+                ProductId = 1,
+                Name = "Product 1",
+                Price = 100,
+                Count = 10
+            };
+            Product product2 = new Product()
+            {
+                ProductId = 2,
+                Name = "Product 2",
+                Price = 200,
+                Count = 20
+            };
+            Cart cart = new Cart(customer);
+            var expectedResult = new List<Product>()
+            {
+                product1, product1, product2
+            };
+
+            cart.Add(product1);
+            cart.Add(product1);
+            cart.Add(product2);
+
+            var cartResult = cart.GetAll();
+
+            Assert.AreEqual(expectedResult.Count, cartResult.Count);
+            for (int i = 0; i < expectedResult.Count; ++i)
+            {
+                Assert.AreEqual(expectedResult[i], cartResult[i]);
+            }
+        }
+    }
+}
