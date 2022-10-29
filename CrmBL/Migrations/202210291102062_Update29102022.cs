676ce68db4f1c4404c6b278c15678e4448f757d4
DenysKachalenko <kachalenko.denis@gmail.com>
2022-10-29 14:04:23 +0300

    Add a new migration.

---
+++ CrmBL/Migrations/202210291102062_Update29102022.cs
@@ -0,0 +1,18 @@
+﻿namespace CrmBL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update29102022 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Checks", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Checks", "Price");
+        }
+    }
+}
